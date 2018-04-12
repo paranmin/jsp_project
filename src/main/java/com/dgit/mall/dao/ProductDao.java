988dd3491ebe3bc0a24c6db1af2bd9d35c331289
EunAe Kim <eunae0474@naver.com>
2018-04-12 15:46:07 +0900

    상품 옵션 등록 완료

--- src/main/java/com/dgit/mall/dao/ProductDao.java
+++ src/main/java/com/dgit/mall/dao/ProductDao.java
@@ -3,6 +3,7 @@ package com.dgit.mall.dao;
 import java.sql.SQLException;
 
 import com.dgit.mall.dto.Option;
+import com.dgit.mall.dto.OptionDetail;
 import com.dgit.mall.dto.Product;
 import com.dgit.mall.dto.Proimg;
 
@@ -10,6 +11,7 @@ public interface ProductDao {
 	public int insertProduct(Product pro) throws SQLException;
 	public int insertProImg(Proimg img) throws SQLException;
 	public int insertOption(Option opt) throws SQLException;
+	public int insertOptionDetail(OptionDetail det) throws SQLException;
 	public int selectLastInsert() throws SQLException;
-	
+	public int selectLastInsertOption() throws SQLException;
 }
