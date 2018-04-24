4812e1117fcbb89f55e204f1018ae714f925e710
EunAeKim <eunae0474@naver.com>
2018-04-24 17:47:12 +0900

    리스트들 틀만들기

---
+++ src/main/java/com/dgit/mall/handler/shop/product/ProductSelectListHandler.java
@@ -0,0 +1,16 @@
+package com.dgit.mall.handler.shop.product;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dgit.mall.handler.shop.ShopCommandHandler;
+
+public class ProductSelectListHandler extends ShopCommandHandler {
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
